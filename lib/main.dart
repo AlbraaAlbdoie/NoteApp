a150aff7144afb27795e279b5a170127ce7eac5e
Albraa Adel Albdoie <AlbraaAlbdoie@gmail.com>
2025-01-20 15:09:57 +0300

    creat notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/note_view.dart';
 
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NoteView(),
+    );
   }
 }
